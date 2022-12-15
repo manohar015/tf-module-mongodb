66875d144f740f15781dfc470275f3eafa094fa0
Manohar <manohar015@gmail.com>
2022-12-15 19:55:02 +0530

    Added my Comments

--- data.tf
+++ data.tf
@@ -4,5 +4,7 @@ data "terraform_remote_state" "vpc" {
     bucket = "manohar-b50-tf-state-bucket"
     key    = "vpc/${var.ENV}/terrafom.tfstate"
     region = "us-east-1"
+    access_key = "AKIAYWHE32Q7SPNV4KM5"
+    secret_key = "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"
   }
 }
