bac19ee2383b8d98f388ae57b9801093e026e603
Manohar <manohar015@gmail.com>
2022-12-15 15:17:45 +0530

    Added my Comments

--- cluster.tf
+++ cluster.tf
@@ -1,4 +1,4 @@
-resource "aws_docdb_cluster" "docdb" {
+/* resource "aws_docdb_cluster" "docdb" {
   cluster_identifier      = "roboshop-${var.ENV}"
   engine                  = "docdb"
   master_username         = admin1
@@ -7,7 +7,7 @@ resource "aws_docdb_cluster" "docdb" {
   skip_final_snapshot     = true
   db_subnet_group_name    = aws_docdb_subnet_group.docdb.name
   vpc_security_group_ids  = [aws_security_group.allow_docdb.id]
-}
+} */
 
 # Creates Subnet Group
 resource "aws_docdb_subnet_group" "docdb" {
