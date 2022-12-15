1c6e9e8d9f82c8aad013d1eba459531fbcc48159
Manohar <manohar015@gmail.com>
2022-12-15 15:49:07 +0530

    Added my Comments

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "b50-tf-state-bucket"
+    bucket = "manohar-b50-tf-state-bucket"
     key    = "vpc/${var.ENV}/terrafom.tfstate"
     region = "us-east-1"
   }
