c4e79daad06ec7d27f90eec1ce88ba83ad1ca577
Manohar <manohar015@gmail.com>
2022-12-17 13:38:37 +0530

    Added my Comments

--- data.tf
+++ data.tf
@@ -6,3 +6,15 @@ data "terraform_remote_state" "vpc" {
     region = "us-east-1"
   }
 }
+
+data "aws_secretsmanager_secret" "secrets" {
+  name = "roboshop/secrets"
+}
+
+data "aws_secretsmanager_secret_version" "secrets" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
+
+# output "example" {
+#    value = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_USERNAME"]
+# }
